16adea0033789904675ac63ec45ea7a4f2cfc822
abhays07 <itsrajputabhaysingh997@gmail.com>
2026-03-07 16:01:56 +0530

    [Abhay Singh Lodhi] : UC3 - Edit existing contact person using person's name through console input

--- src/main/java/com/addressbook/AddressBook.java
+++ src/main/java/com/addressbook/AddressBook.java
@@ -1,37 +1,81 @@
 package com.addressbook;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class AddressBook {
+	ArrayList<ContactPerson> contactList = new ArrayList<>();
+	Scanner scanner = new Scanner(System.in);
+
+	
 	public void addContact() {
-		Scanner scanner = new Scanner(System.in);
-		 ContactPerson person = new ContactPerson();
 
-	        System.out.print("Enter First Name: ");
-	        person.firstName = scanner.nextLine();
+		ContactPerson person = new ContactPerson();
 
-	        System.out.print("Enter Last Name: ");
-	        person.lastName = scanner.nextLine();
+		System.out.print("Enter First Name: ");
+		person.firstName = scanner.nextLine();
 
-	        System.out.print("Enter Address: ");
-	        person.address = scanner.nextLine();
+		System.out.print("Enter Last Name: ");
+		person.lastName = scanner.nextLine();
 
-	        System.out.print("Enter City: ");
-	        person.city = scanner.nextLine();
+		System.out.print("Enter Address: ");
+		person.address = scanner.nextLine();
 
-	        System.out.print("Enter State: ");
-	        person.state = scanner.nextLine();
+		System.out.print("Enter City: ");
+		person.city = scanner.nextLine();
 
-	        System.out.print("Enter Zip: ");
-	        person.zip = scanner.nextInt();
-	        scanner.nextLine();
+		System.out.print("Enter State: ");
+		person.state = scanner.nextLine();
 
-	        System.out.print("Enter Phone Number: ");
-	        person.phoneNumber = scanner.nextLine();
+		System.out.print("Enter Zip: ");
+		person.zip = scanner.nextInt();
+		scanner.nextLine();
 
-	        System.out.print("Enter Email: ");
-	        person.email = scanner.nextLine();
+		System.out.print("Enter Phone Number: ");
+		person.phoneNumber = scanner.nextLine();
 
-	        System.out.println("Contact Added Successfully");
+		System.out.print("Enter Email: ");
+		person.email = scanner.nextLine();
+
+		contactList.add(person);
+		System.out.println("Contact Added Successfully");
+	}
+
+	public void editContact() {
+		System.out.println("Enter First Name of Contact to Edit:  ");
+		String name = scanner.nextLine();
+
+		boolean found = false;
+		
+		for (ContactPerson person : contactList) {
+			if (person.firstName.equalsIgnoreCase(name)) {
+				System.out.print("Enter New Address: ");
+				person.address = scanner.nextLine();
+
+				System.out.print("Enter New City: ");
+				person.city = scanner.nextLine();
+
+				System.out.print("Enter New State: ");
+				person.state = scanner.nextLine();
+
+				System.out.print("Enter New Zip: ");
+				person.zip = scanner.nextInt();
+				scanner.nextLine();
+
+				System.out.print("Enter New Phone Number: ");
+				person.phoneNumber = scanner.nextLine();
+
+				System.out.print("Enter New Email: ");
+				person.email = scanner.nextLine();
+
+				System.out.println("Contact Updated Successfully");
+				found=true;
+				break;
+			}
+
+		}
+		if(!found) {
+			System.out.println("Contact Not Found");
+		}
 	}
 }
