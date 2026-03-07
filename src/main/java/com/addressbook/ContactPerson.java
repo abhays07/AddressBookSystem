cb8cc3b3def1c2bba790d956aaeb29ed2d8ad24f
abhays07 <itsrajputabhaysingh997@gmail.com>
2026-03-07 15:08:23 +0530

    [Abhay Singh Lodhi] : UC1 - Create Contact Person Class

---
+++ src/main/java/com/addressbook/ContactPerson.java
@@ -0,0 +1,13 @@
+package com.addressbook;
+
+public class ContactPerson {
+	String firstName;
+    String lastName;
+    String address;
+    String city;
+    String state;
+    int zip;
+    String phoneNumber;
+    String email;
+
+}
