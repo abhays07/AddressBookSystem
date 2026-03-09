400444d28e694b200c7b031467015f7422db7bb9
abhays07 <itsrajputabhaysingh997@gmail.com>
2026-03-09 10:09:23 +0530

    [Abhay Singh Lodhi] : UC6 - Add multiple AddressBooks using HashMap dictionary mapping AddressBook name to AddressBook instance

--- src/main/java/com/addressbook/AddressBookMain.java
+++ src/main/java/com/addressbook/AddressBookMain.java
@@ -1,5 +1,6 @@
 package com.addressbook;
 
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class AddressBookMain {
@@ -8,45 +9,91 @@ public class AddressBookMain {
 
         System.out.println("Welcome to Address Book Program");
 
-        AddressBook addressBook = new AddressBook();
+        HashMap<String, AddressBook> addressBookMap = new HashMap<>();
+
         Scanner scanner = new Scanner(System.in);
 
-        boolean running = true;
+        while (true) {
 
-        while (running) {
-
-            System.out.println("\n1. Add Contact");
-            System.out.println("2. Edit Contact");
-            System.out.println("3. Delete Contact");
-            System.out.println("4. Display Contacts");
-            System.out.println("5. Exit");
+            System.out.println("\n1. Create New Address Book");
+            System.out.println("2. Select Address Book");
+            System.out.println("3. Exit");
 
             System.out.print("Enter Choice: ");
-
             int choice = scanner.nextInt();
             scanner.nextLine();
 
             switch (choice) {
 
                 case 1:
-                    addressBook.addContact();
+
+                    System.out.print("Enter Address Book Name: ");
+                    String name = scanner.nextLine();
+
+                    addressBookMap.put(name, new AddressBook());
+
+                    System.out.println("Address Book Created Successfully");
+
                     break;
 
                 case 2:
-                    addressBook.editContact();
+
+                    System.out.print("Enter Address Book Name: ");
+                    String bookName = scanner.nextLine();
+
+                    AddressBook addressBook = addressBookMap.get(bookName);
+
+                    if (addressBook == null) {
+                        System.out.println("Address Book Not Found");
+                        break;
+                    }
+
+                    boolean running = true;
+
+                    while (running) {
+
+                        System.out.println("\n1 Add Contact");
+                        System.out.println("2 Edit Contact");
+                        System.out.println("3 Delete Contact");
+                        System.out.println("4 Display Contacts");
+                        System.out.println("5 Back");
+
+                        System.out.print("Enter Choice: ");
+
+                        int option = scanner.nextInt();
+                        scanner.nextLine();
+
+                        switch (option) {
+
+                            case 1:
+                                addressBook.addContact();
+                                break;
+
+                            case 2:
+                                addressBook.editContact();
+                                break;
+
+                            case 3:
+                                addressBook.deleteContact();
+                                break;
+
+                            case 4:
+                                addressBook.displayContacts();
+                                break;
+
+                            case 5:
+                                running = false;
+                                break;
+
+                            default:
+                                System.out.println("Invalid Choice");
+                        }
+                    }
+
                     break;
 
                 case 3:
-                    addressBook.deleteContact();
-                    break;
-
-                case 4:
-                    addressBook.displayContacts();
-                    break;
-
-                case 5:
-                    running = false;
-                    break;
+                    System.exit(0);
 
                 default:
                     System.out.println("Invalid Choice");
